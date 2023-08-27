56e1df4573d178441c7330e176e17c8e8a8b4356
machao <frmachao@126.com>
2023-08-27 22:25:11 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ofa.js-todos
+
+## quick start
+
+To start a web server to access index.html
---
+++ data.mjs
@@ -0,0 +1,4 @@
+export const data = $.stanz({
+  todos: [{ id: 0, status: 1, text: "eat!" }],
+  status: { 0: "In Progress", 1: "Done" },
+});
